39533fbb905a945463467547512da90d9e26eb18
veezex <veezex@denero.ru>
2018-08-22 12:09:25 +0300

    верска dropzone

--- src/js/partials/form-file-input.js
+++ src/js/partials/form-file-input.js
@@ -14,11 +14,13 @@ $('.dropzone-images').each(function() {
         paramName: 'file', // The name that will be used to transfer the file
         url: area.data('upload'),
         dictRemoveFile: 'Удаление фотографии',
-        dictDefaultMessage: 'Перетащите сюда фотографии или кликните для загрузки файлов<br><br>\
+        dictDefaultMessage: 'Перетащите сюда фотографии или кликните<br> для загрузки файлов<br><br>\
             (максимум ' + area.data('maxfiles') + ' фотографий)',
         dictFileTooBig: 'Превышен допустимый размер файла: {{maxFilesize}} Мб',
         dictMaxFilesExceeded: 'Превышено максимальное кол-во фотографий для загрузки: {{maxFiles}}',
         dictInvalidFileType: 'Вы не можете добавить файлы этого типа',
+        dictCancelUpload: 'Отменить загрузку',
+        dictUploadCanceled: 'Загрузка отменена',
         maxFilesize: 2, // mb
         success: function(file, response) {
             var pe = $(file.previewElement);
