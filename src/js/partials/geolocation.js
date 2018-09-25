ddd81c37d71791a9196ffc4b99f56b9651bd14d0
Oleg Sobolev <osobolev@dv-bc.ru>
2018-09-25 21:27:56 +0500

    Resolve sort cities by region problem. Added func for city search

--- src/js/partials/geolocation.js
+++ src/js/partials/geolocation.js
@@ -20,6 +20,9 @@ $(document).ready(function(){
       $('.current-city-choose').removeClass('current-city-choose--active');
       $('.modal-item').addClass('modal-item--active');
       $('body').addClass('overflow');
+
+      regionsLoad();
+      citiesLoad();
     });
 
     $('.close-modal').click(function(e) {
@@ -34,152 +37,175 @@ $(document).ready(function(){
       $('body').removeClass('overflow');
     })
 
-    var cities = window.config.geoCities;
-    var regions = window.config.geoRegions;
-
     function asc_sort(a, b){
       return (b.name) < (a.name) ? 1 : -1;
     }
 
     // загружаем список регионов
-    function regionsLoad(){
-      $.ajax({
-        url: regions,
-        dataType : "json",
-        success: function (data) {
-          data.sort(asc_sort);
-          var regionsArray = [];
+    function regionsLoad() {
+      var regions;
+      if (!localStorage.getItem('regions')) { // если locaStorage пустой, отправляем запрос, записываем данные
+        $.ajax({
+          url: window.config.geoRegions,
+          dataType: "json",
+          success: function (data) {
+            localStorage.setItem('regions', JSON.stringify(data));
+            regionsRender(data);
+            console.log(data);
+          }
+        });
+      } else {
+        regions = JSON.parse(localStorage.getItem('regions'));
+        regionsRender(regions);
+      }
+    }
 
-          $.each(data, function(i, val) {
-            var item = {};
-            var letter = val.name.charAt(0);
-            item.title = letter;
-            item.items = data.filter(item => item.name.charAt(0) == letter);
-            regionsArray.push(item);
-          });
+    // отображаем список регионов
+    function regionsRender(regions) {
+      regions.sort(asc_sort);
+      var regionsArray = [];
 
-         var nextRegions = regionsArray.sort(function(a,b){return a.title < b.title ? -1 : 1;}).reduce(function(arr, el){
-            if(!arr.length || arr[arr.length - 1].title != el.title) {
-              arr.push(el);
-            }
-            return arr;
-          }, []);
+      $.each(regions, function(i, val) {
+        var item = {};
+        var letter = val.name.charAt(0);
+        item.title = letter;
+        item.items = regions.filter(item => item.name.charAt(0) == letter);
+        regionsArray.push(item);
+      });
 
-          $.each(nextRegions, function(i, val) {
-            $('.all-regions').append(
-              '<div class="region-row"><div class="first-letter">' + val.title + '</div><ul class="region-list" data-letter="' + val.title + '"></ul></div>'
-            );
-
-            $.each(val.items, function(i, val) {
-              var letter = val.name.charAt(0);
-              $('.region-list[data-letter="' + letter + '"]').append(
-                '<li class="region" data-region="' + val.id + '"><span>' + val.name + '</span></li>'
-              );
-            });
-          });
-
-          $('.region').click(function(){
-            var id = $(this).data('region');
-            $('.cities').html('');
-            $.ajax({
-              url: cities,
-              dataType : "json",
-              success: function (data) {
-                data.sort(asc_sort);
-                var citiesArray = [];
-
-                $.each(data, function(i, val) {
-                  if(val.rid == id) {
-                    var item = {};
-                    var letter = val.name.charAt(0);
-                    item.title = letter;
-                    item.items = data.filter(item => item.name.charAt(0) == letter);
-                    citiesArray.push(item);
-                  }
-                });
-
-                var nextCities = citiesArray.sort(function(a,b){return a.title < b.title ? -1 : 1;}).reduce(function(arr, el){
-                  if(!arr.length || arr[arr.length - 1].title != el.title) {
-                    arr.push(el);
-                  }
-                  return arr;
-                }, []);
-
-                $.each(nextCities, function(i, val) {
-                  $('.cities').append(
-                    '<div class="cities-row"><div class="first-letter">' + val.title + '</div><ul class="cities-list" data-letter="' + val.title + '"></ul></div>'
-                  )
-
-                  $.each(val.items, function(i, val) {
-                    var letter = val.name.charAt(0);
-                    $('.cities-list[data-letter="'+ letter +'"]').append(
-                      '<li class="city" data-region="' + val.id + '"><span>' + val.name + '</span></li>'
-                    )
-
-                    //выбераем город
-                    $('.city').click(function(){
-                      var city = $(this).text();
-                      $('.current-city').text(city);
-                      $('.modal-item').removeClass('modal-item--active');
-                    });
-                  });
-                });
-              }
-            });
-          });
+      var nextRegions = regionsArray.sort(function(a,b){return a.title < b.title ? -1 : 1;}).reduce(function(arr, el){
+        if(!arr.length || arr[arr.length - 1].title != el.title) {
+          arr.push(el);
         }
+        return arr;
+      }, []);
+
+      $.each(nextRegions, function(i, val) {
+        $('.all-regions').append(
+          '<div class="region-row"><div class="first-letter">' + val.title + '</div><ul class="region-list" data-letter="' + val.title + '"></ul></div>'
+        );
+
+        $.each(val.items, function(i, val) {
+          var letter = val.name.charAt(0);
+          $('.region-list[data-letter="' + letter + '"]').append(
+            '<li class="region" data-region="' + val.id + '"><span>' + val.name + '</span></li>'
+          );
+        });
+      });
+
+      $('.region').click(function(){
+        var id = $(this).data('region');
+        var citiesData = JSON.parse(localStorage.getItem('cities'));
+        $('.cities').html('');
+        citiesByRegion(citiesData, id);
       });
     }
 
+    function citiesLoad() {
+      var cities;
+      if (!localStorage.getItem('cities')) { // если locaStorage пустой, отправляем запрос, записываем данные
+        $.ajax({
+          url: window.config.geoCities,
+          dataType: "json",
+          success: function (data) {
+            localStorage.setItem('cities', JSON.stringify(data));
+            citiesRender(data);
+          }
+        });
+      } else {
+        cities = JSON.parse(localStorage.getItem('cities'));
+        citiesRender(cities);
+      }
+    }
+
     // загружаем список городов
-    function citiesLoad(){
-      $.ajax({
-        url: cities,
-        dataType : "json",
-        success: function (data) {
-          data.sort(asc_sort);
-          var citiesArray = [];
+    function citiesRender(cities) {
+      cities.sort(asc_sort);
+      var citiesArray = [];
 
-          $.each(data, function(i, val) {
-            var item = {};
-            var letter = val.name.charAt(0);
-            item.title = letter;
-            item.items = data.filter(item => item.name.charAt(0) == letter);
-            citiesArray.push(item);
-          });
+      $.each(cities, function(i, val) {
+        var item = {};
+        var letter = val.name.charAt(0);
+        item.title = letter;
+        item.items = cities.filter(item => item.name.charAt(0) == letter);
+        citiesArray.push(item);
+      });
 
-          var nextCities = citiesArray.sort(function(a,b){return a.title < b.title ? -1 : 1;}).reduce(function(arr, el){
-            if(!arr.length || arr[arr.length - 1].title != el.title) {
-              arr.push(el);
-            }
-            return arr;
-          }, []);
+      var nextCities = citiesArray.sort(function(a,b){return a.title < b.title ? -1 : 1;}).reduce(function(arr, el){
+        if(!arr.length || arr[arr.length - 1].title != el.title) {
+          arr.push(el);
+        }
+        return arr;
+      }, []);
 
-          $.each(nextCities, function(i, val) {
-            $('.cities').append(
-              '<div class="cities-row"><div class="first-letter">' + val.title + '</div><ul class="cities-list" data-letter="' + val.title + '"></ul></div>'
-            )
+      $.each(nextCities, function(i, val) {
+        $('.cities').append(
+          '<div class="cities-row"><div class="first-letter">' + val.title + '</div><ul class="cities-list" data-letter="' + val.title + '"></ul></div>'
+        )
 
-            $.each(val.items, function(i, val) {
-              var letter = val.name.charAt(0);
-              $('.cities-list[data-letter="'+ letter +'"]').append(
-                '<li class="city" data-region="' + val.id + '"><span>' + val.name + '</span></li>'
-              )
-            });
-          });
+        $.each(val.items, function(i, val) {
+          var letter = val.name.charAt(0);
+          $('.cities-list[data-letter="'+ letter +'"]').append(
+            '<li class="city" data-region="' + val.id + '"><span>' + val.name + '</span></li>'
+          )
+        });
+      });
+
+      //выбераем город
+      $('.city').click(function () {
+        var city = $(this).text();
+        $('.current-city').text(city);
+        $('.modal-item').removeClass('modal-item--active');
+      });
+    }
+
+    // отображаем города в зависимости от региона
+    function citiesByRegion(cities, id) {
+      cities.sort(asc_sort);
+      var filteredCities = [];
+      var citiesArray = [];
+
+      $.each(cities, function(i, val) {
+        if (val.rid == id) {
+          filteredCities.push(val);
+        }
+      });
+
+      $.each(filteredCities, function (i, val) {
+          var item = {};
+          var letter = val.name.charAt(0);
+          item.title = letter;
+          item.items = filteredCities.filter(item => item.name.charAt(0) == letter);
+          citiesArray.push(item);
+      })
+
+      var nextCities = citiesArray.sort(function(a,b){return a.title < b.title ? -1 : 1;}).reduce(function(arr, el){
+        if(!arr.length || arr[arr.length - 1].title != el.title) {
+          arr.push(el);
+        }
+        return arr;
+      }, []);
+
+      $.each(nextCities, function(i, val) {
+        $('.cities').append(
+          '<div class="cities-row"><div class="first-letter">' + val.title + '</div><ul class="cities-list" data-letter="' + val.title + '"></ul></div>'
+        )
+
+        $.each(val.items, function(i, val) {
+          var letter = val.name.charAt(0);
+          $('.cities-list[data-letter="'+ letter +'"]').append(
+            '<li class="city" data-region="' + val.id + '"><span>' + val.name + '</span></li>'
+          )
 
           //выбераем город
           $('.city').click(function(){
             var city = $(this).text();
             $('.current-city').text(city);
             $('.modal-item').removeClass('modal-item--active');
-          })
-        }
+          });
+        });
       });
     }
-
-    regionsLoad();
-    citiesLoad();
 
     // Открываем выбор города
     $('.current-city').click(function(){
@@ -191,4 +217,15 @@ $(document).ready(function(){
       $('.cities').html('');
       citiesLoad();
     });
-  });
+
+  $('.city-search-input').on('input', function () {
+    var cityValue = $(this).val();
+    $.ajax({
+      url: window.config.geoCities + '?q='+ cityValue +'',
+      dataType: "json",
+      success: function (data) {
+        citiesRender(data);
+      }
+    });
+  })
+});
